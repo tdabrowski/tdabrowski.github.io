49973e649068aea084661b5de5f4372914623a6f
tdabrowski <dabr@poczta.fm>
2018-02-16 17:33:09 +0100

    adding updated Chuck files

--- Chuck/js/app.js
+++ Chuck/js/app.js
@@ -1,11 +1,11 @@
 $(function(){
 
     let $title = $('.title');
-    let $url = 'https://api.icndb.com/jokes/random';
+    let $url = 'http://api.icndb.com/jokes/random';
     let $button = $('.generate');
 
     function responseHandler(response,element){
-        let $joke = $('<h2 class="joke">'+response.value.joke+'</h2>');
+        let $joke = $('<h2 class="joke content">'+response.value.joke+'</h2>');
         element.after($joke);
     }
 
@@ -14,6 +14,7 @@ $(function(){
         	url:path,
         	type:'GET',
         }).done(function(response){
+            element.next().remove();
             responseFunction(response,element);
         }).fail(function(error){
         	console.log('Connection error - status: ' + error.statusText);
@@ -23,7 +24,7 @@ $(function(){
     loadContent($url,responseHandler,$title);
 
     $button.on('click',(e)=>{
-        $title.next().remove();
+        e.preventDefault();
         loadContent($url,responseHandler,$title);
     });
 
