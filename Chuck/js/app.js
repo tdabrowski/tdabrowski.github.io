448c6cd36dd71e37d276d9835bb9ff35adaf0ef1
tdabrowski <dabr@poczta.fm>
2018-02-19 18:40:13 +0100

    updated Chuck and SitOnChair files

--- Chuck/js/app.js
+++ Chuck/js/app.js
@@ -21,11 +21,17 @@ $(function(){
         });
     }
 
-    loadContent($url,responseHandler,$title);
+    let loop = setInterval(()=>{
+        loadContent($url,responseHandler,$title);
+    },10000);
 
     $button.on('click',(e)=>{
         e.preventDefault();
+        clearInterval(loop);
         loadContent($url,responseHandler,$title);
+        loop = setInterval(()=>{
+            loadContent($url,responseHandler,$title);
+        },10000);
     });
 
 
